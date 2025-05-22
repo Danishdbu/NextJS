d0a3ee3f6bf8aae0aeb662b564040d30be9547e4
Danishdbu <mohddanishdbu@gmail.com>
2025-05-22 11:56:08 +0530

    basic

---
+++ file-1/src/app/notFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default  notFound = () => {
+  return (
+    <div>
+      <h1>404</h1>
+    </div>
+  )
+}
+
+
---
+++ file-1/src/app/page.jsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main>
+    <h1>Hello jiiii</h1>
+    </main>
+  );
+}
