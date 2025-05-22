079c61318b11118bbe72e401f158e6f318c6d159
Danishdbu <mohddanishdbu@gmail.com>
2025-05-22 12:27:56 +0530

    file based routing

---
+++ file-2/src/app/performence/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Performence = () => {
+  return (
+    <div>performemce Pages</div>
+  )
+}
+
+export default Performence
---
+++ file-2/src/app/reliability/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Reliability = () => {
+  return (
+    <div>reliability pages</div>
+  )
+}
+
+export default Reliability
