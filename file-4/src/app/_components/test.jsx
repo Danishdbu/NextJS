d95e4d25d5ac4e6afaa8da8690b3a385d7c14927
Danishdbu <mohddanishdbu@gmail.com>
2025-06-02 14:47:09 +0530

    Private Folders in Next.js

---
+++ file-4/src/app/_components/test.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TestComponent = () => {
+  return (
+    <>
+    <h1>testing phase</h1>
+    </>
+    
+  )
+}
+
+export default TestComponent
--- file-4/src/app/page.js
+++ file-4/src/app/page.js
@@ -1,10 +1,10 @@
 import Link from "next/link";
-
+import TestComponent from "./_components/test";
 export default function Home() {
   return (
     <>
       <h1>Technical Agency</h1>
-      
+      <TestComponent/>
       <p>
         <Link href="/about">About</Link>
       </p>{" "}
