9d6a299c4ee1e0933ef412afa661899ad55aa618
Danishdbu <mohddanishdbu@gmail.com>
2025-06-04 12:24:49 +0530

    data fetching

---
+++ file-7/src/app/todos/loading.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+     <>
+      <h1>Todos</h1>
+      <div className="todos-container">
+        {Array.from({ length: 5 }).map((_, index) => (
+          <li key={index} className="shimmer">
+            <div className="shimmer-checkbox"></div>
+            <div className="shimmer-text"></div>
+          </li>
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default loading
--- file-7/src/app/todos/page.jsx
+++ file-7/src/app/todos/page.jsx
@@ -1,10 +1,15 @@
-const Posts = async () => {
+import { Suspense } from 'react'
 
+const Posts = async () => {
+const slowResponse = await fetch("https://procodrr.vercel.app/?sleep=2000");
+const data = await slowResponse.json();
+console.log(data)
 const response = await fetch("https://jsonplaceholder.typicode.com/todos?_limit=5");
 
 const todos = await response.json();
   return (
     <>
+    
       <h1>Todos</h1>
       <div className="todos-conatiner">
         {todos.map(({id,title,completed}) =>(
@@ -15,6 +20,7 @@ const todos = await response.json();
         ))}
 
       </div>
+      
       
     </>
   );
